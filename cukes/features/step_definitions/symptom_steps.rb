09013e955d290ac7e21a6ba52be66d5a5111a5e9
Kyle McKee <mckee.kyle@gmail.com>
2016-01-20 22:58:15 -0500

    Renamed step file

--- cukes/features/step_definitions/symptom_steps.rb
+++ cukes/features/step_definitions/symptom_steps.rb
@@ -2,8 +2,13 @@ Given(/^I have had no symptoms lately$/) do
     # No Op
 end
 
+Given(/^I have had the following symptoms:$/) do |symptom_table|
+    symptom_table.symbolic_hashes.each do |symptom|
+        FactoryGirl.create(:symptom, symptom)
+    end
+end
+
 When(/^I view my symptoms$/) do
-    puts 'about to visit'
     visit(SymptomIndexPage)
 end
 
@@ -13,12 +18,6 @@ end
 
 Then(/^I should see text explaining the what and why of symptoms$/) do
     expect(on(SymptomIndexPage).explanation_visible?).to be true
-end
-
-Given(/^I have had the following symptoms:$/) do |symptom_table|
-    symptom_table.symbolic_hashes.each do |symptom|
-        puts FactoryGirl.create(:symptom, symptom)
-    end
 end
 
 Then(/^I should see the following symptoms:$/) do |expected_symptoms|
