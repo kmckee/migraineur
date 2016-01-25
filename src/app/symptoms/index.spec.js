787a0324dcbafea0746c69b55a453782a59084eb
Kyle McKee <mckee.kyle@gmail.com>
2016-01-25 08:09:02 -0500

    Don't need the sinon sandbox per docs.

--- src/app/symptoms/index.spec.js
+++ src/app/symptoms/index.spec.js
@@ -1,15 +1,12 @@
 describe('SymptomsIndexCtrl', function() {
-    var symptomRepository, createController, $scope, sandbox, $q, $rootScope;
+    var symptomRepository, createController, $scope, $q, $rootScope;
     beforeEach(module('starter'));
     beforeEach(module('templates'));
     beforeEach(inject(function($controller, _$rootScope_, _$q_) {
-        sandbox = sinon.sandbox.create();
         $q = _$q_;
-        symptomRepository = {
-            all: function() {}
-        };
         $rootScope = _$rootScope_;
         $scope = $rootScope.$new();
+        symptomRepository = { all: function() {} };
         createController = function() {
             return $controller('SymptomsCtrl', {
                 $scope: $scope,
@@ -18,9 +15,6 @@ describe('SymptomsIndexCtrl', function() {
         };
 
     }));
-    afterEach(function() {
-        sandbox.restore();
-    });
     it('gets all the symptoms', function() {
         sinon.stub(symptomRepository, 'all').returns($q.when());
         createController();
